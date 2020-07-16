7cbf3957fb327643bb80d217bce11d027c0a927e
Michael Knox <knoxm1@gmail.com>
2020-07-16 11:05:26 -0500

    updates

--- s3/s3_test/main.tf
+++ s3/s3_test/main.tf
@@ -1,6 +1,7 @@
 module "s3_bucket" {
-  source              = "./modules"
+#  source              = "./modules"
+  source              = "git::git@github.com:empyrean987/Terraform.git//s3/s3_test/modules?ref=v0.0.12"
   bucket              = var.bucket
 }
 
-variable "bucket" {}
+#variable "bucket" {}
