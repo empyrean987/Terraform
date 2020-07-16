e2c47349b0a4874465ed475a2ec0708ce1877748
Michael Knox <knoxm1@gmail.com>
2020-07-16 11:33:14 -0500

    updates

--- s3/s3_test/main.tf
+++ s3/s3_test/main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region = "us-east-1"
+  # ... other provider settings ...
+}
+
 module "s3_bucket" {
 #  source              = "./modules"
   source              = "git::git@github.com:empyrean987/Terraform.git//s3/s3_test/modules/s3_bucket?ref=v0.0.16"
